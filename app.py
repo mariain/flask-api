56d5a2cf8dbe54f831c1a303b19f533dc50dd9e3
Maria <mariain@llee-mac.local>
2018-03-26 16:11:39 -0700

    Initial API development

---
+++ app.py
@@ -0,0 +1,157 @@
+from models import Base, User, Post
+from flask import Flask, jsonify, request, url_for, abort, g
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+from flask_httpauth import HTTPBasicAuth
+
+from datetime import datetime
+import math, os
+
+per_page = 5
+auth = HTTPBasicAuth()
+
+engine = create_engine(os.environ.get('FLASK_API_DB'))
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+app = Flask(__name__)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5001'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT, PATCH'
+        response.headers['Access-Control-Allow-Headers'] = 'authorization'
+    return response
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    #Try to see if it's a token first
+    user_id = User.verify_auth_token(username_or_token)
+    if user_id:
+        user = session.query(User).filter_by(id = user_id).one()
+    else:
+        user = session.query(User).filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+    
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    print(g.user.username)
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
+
+@app.route('/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    email = request.json.get('email')
+    password = request.json.get('password')
+    if username is None or password is None:
+        print("missing arguments")
+        abort(400) 
+        
+    if session.query(User).filter_by(username = username).first() is not None:
+        print("existing user")
+        user = session.query(User).filter_by(username=username).first()
+        return jsonify({'message':'user already exists'}), 200#, {'Location': url_for('get_user', id = user.id, _external = True)}
+        
+    user = User(username = username, email = email)
+    user.hash_password(password)
+    session.add(user)
+    session.commit()
+    return jsonify({ 'username': user.username }), 201#, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+@app.route('/users/<username>')
+def get_user(username):
+    user = session.query(User).filter_by(username = username).one()
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username, 'about': user.about, 'avatar': "http://" + request.host + "/static/images/" + user.avatar  })
+
+@app.route('/users/me', methods = ['GET', 'PUT'])
+@auth.login_required
+def updateSetting():
+    user = session.query(User).filter_by(id = g.user.id).one()
+    if request.method == 'PUT':       
+        user.username = request.json.get('username')
+        user.about = request.json.get('about')
+        session.add(user)
+        session.commit()
+        return "Updated a User with id %s" % id
+    if request.method == 'GET':
+        return jsonify({'username': user.username, 'about': user.about, 'avatar': "http://" + request.host + "/static/images/" + user.avatar})
+
+
+@app.route('/users/me/avatar', methods = ['PUT'])
+@auth.login_required
+def updateAvatar():
+    if request.method == 'PUT':
+        user = session.query(User).filter_by(id = g.user.id).one()
+        user.avatar = request.json.get('username')
+
+        session.add(user)
+        session.commit()
+        return "Updated a User with id %s" % id
+
+@app.route('/users/<username>/posts', methods = ['GET'])
+@auth.login_required
+def showUserPosts(username):
+    if request.method == 'GET':
+        current_page = request.args.get('page', '')
+        if not current_page:
+            current_page = 1
+        if float(current_page).is_integer() == False:
+            current_page = 1
+        current_page = int(current_page)
+        user = session.query(User).filter_by(username = username).one()
+        posts = session.query(Post).filter_by(user_id = user.id).order_by(Post.created_at.desc())[(current_page - 1) * per_page : current_page * per_page ]
+        total = session.query(Post).filter_by(user_id = user.id).count()
+        return jsonify({'current_page': current_page, 'last_page': math.ceil(total/per_page), 'total': total, 'data': [p.serialize for p in posts]})
+
+
+@app.route('/posts', methods = ['GET', 'POST'])
+@auth.login_required
+def showAllPosts():
+    if request.method == 'GET':
+        current_page = request.args.get('page', '')
+        if not current_page:
+            current_page = 1
+        if float(current_page).is_integer() == False:
+            current_page = 1
+        current_page = int(current_page)
+        posts = session.query(Post).order_by(Post.created_at.desc())[(current_page - 1) * per_page : current_page * per_page ]
+        total = session.query(Post).count()
+        return jsonify({'current_page': current_page, 'last_page': math.ceil(total/per_page), 'total': total, 'data': [p.serialize for p in posts]})
+            
+    if request.method == 'POST':
+        text = request.json.get('text')
+        newPost = Post(user_id = g.user.id, text = text, created_at = datetime.utcnow())
+        session.add(newPost)
+        session.commit()
+        return jsonify(newPost.serialize)
+
+@app.route('/posts/<int:id>/like', methods = ['PATCH'])
+@auth.login_required
+def likeUnlikePost(id):
+    if request.method == 'PATCH':
+        post = session.query(Post).filter_by(id = id).one()
+        if not post.likes:
+            post.likes = []
+        post.likes = list(post.likes)    
+        if g.user.id in post.likes:
+            post.likes.remove(g.user.id)           
+        else:  
+            post.likes.append(g.user.id)
+        session.add(post)
+        session.commit()
+        return "Updated a Post with id %s" % id
+
+if __name__ == '__main__':
+    app.debug = True
+    #app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    app.run(host='0.0.0.0', port=5000)
---
+++ models.py
@@ -0,0 +1,86 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, ARRAY, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, sessionmaker
+from passlib.apps import custom_app_context as pwd_context
+import random, string, os
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+from flask import request
+
+Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+
+class User(Base):
+    __tablename__ = 'user'
+    __table_args__ = {'schema':'api'}
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+    email = Column(String)
+    avatar = Column(String)
+    about = Column(String)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=3600 * 4):
+        s = Serializer(secret_key, expires_in = expiration)
+        return s.dumps({'id': self.id })
+    
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+        	data = s.loads(token)
+        except SignatureExpired:
+    		#Valid Token, but expired
+            return None
+        except BadSignature:
+    		#Invalid Token
+            return None
+        
+        user_id = data['id']
+        return user_id
+
+class Post(Base):
+    __tablename__ = 'post'
+    __table_args__ = {'schema':'api'}
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("api.user.id"))
+    text = Column(String)
+    likes = Column(ARRAY(Integer))
+    created_at = Column(DateTime)
+
+    user = relationship("User")
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        if (self.likes):
+            return {
+                'id' : self.id,
+                'user_id' : self.user_id,
+                'text' : self.text,
+                'likes' : len(self.likes),
+                'created_at' : self.created_at,
+                'author': {'username': self.user.username, 'avatar': "http://" + request.host + "/static/images/" + self.user.avatar},
+                'liked' : True if self.user_id in self.likes else False
+            }
+        else:
+            return {
+                'id' : self.id,
+                'user_id' : self.user_id,
+                'text' : self.text,
+                'likes' : 0,
+                'created_at' : self.created_at,
+                'author': {'username': self.user.username, 'avatar': "http://" + request.host + "/static/images/" + self.user.avatar},
+                'liked' : False
+            }
+        
+
+
+engine = create_engine(os.environ.get('FLASK_API_DB'))
+Base.metadata.create_all(engine)
